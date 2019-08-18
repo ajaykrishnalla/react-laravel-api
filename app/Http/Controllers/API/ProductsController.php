a4add21dd1c1644cff39da1f07805c240ab9e5b9
Ajay Krishna reddy <ajay.krishna@iwebber.co.in>
2019-08-18 15:03:27 +0530

    passport authentication added

--- app/Http/Controllers/API/ProductsController.php
+++ app/Http/Controllers/API/ProductsController.php
@@ -5,11 +5,16 @@ namespace App\Http\Controllers\API;
 use App\Model\Product;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
+use App\Http\Requests\ProductRequest;
 use App\Http\Resources\Product\ProductResource;
 use App\Http\Resources\Product\ProductCollection;
 
 class ProductsController extends Controller
 {
+    public function __construct(){
+        $this->middleware('auth:api');
+    }
     /**
      * Display a listing of the resource.
      *
@@ -27,9 +32,16 @@ class ProductsController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(ProductRequest $request)
     {
-        //
+        $p = new Product;
+        $p->name = $request->name;
+        $p->detail = $request->description;
+        $p->stock = $request->stock;
+        $p->price = $request->price;
+        $p->discount = $request->discount;
+        $p->save();
+        return response()->json(['data' => new ProductResource($p)], 200);
     }
 
     /**
@@ -50,9 +62,10 @@ class ProductsController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(Request $request, Product $product)
     {
-        //
+        $product->update($request->all());
+        return response()->json(['data' => new ProductResource($product)], 200);
     }
 
     /**
@@ -61,8 +74,10 @@ class ProductsController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function destroy($id)
+    public function destroy(Product $product)
     {
-        //
+        $product->delete();
+        return response()->json(['data' => "Successfully deleted"], 200);
+        
     }
 }
